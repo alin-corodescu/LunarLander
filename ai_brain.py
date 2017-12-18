758f9cf87e6dadca7108656c0f6e660ff81c7c3a
alin-corodescu <alin.corodescu@gmail.com>
2017-12-18 22:33:22 +0200

    fixed bug regarding random actions and implemented batching in the ANN training

--- ai_brain.py
+++ ai_brain.py
@@ -11,14 +11,13 @@ class Brain:
     action_space = None
     observation_space = None
     state = None
-    action = 0
+    action = None
     epsilon = 0.1
-    MAX_BUFFER = 10000
+    MAX_BUFFER = 1000
     model = None
     discount_factor = 0.8
     buffer = []
-    targets = np.array([])
-    states = np.array([])
+
     start_time = int(time.time())
     serialize_file_destination = "log/model"
     serialize_time_interval = 3600
@@ -42,6 +41,7 @@ class Brain:
     def get_action_for(self, current_observation):
         self.state = self.format_input(current_observation)
         if np.random.choice([True, False], 1, p=[self.epsilon, 1 - self.epsilon]):
+            self.action = None
             return self.action_space.sample()
         else:
             # Action_vector is formatted as: [[1,2,3,4]]
@@ -57,9 +57,9 @@ class Brain:
         self.model.add(Dense(100, input_shape=(observation_space.shape[0],)))
         self.model.add(Activation('relu'))
         self.model.add(Dense(action_space.n))
-        self.model.add(Activation('linear'))
+        # self.model.add(Activation('linear'))
 
-        optimizer = SGD(lr=0.2, momentum=0.0)
+        optimizer = SGD(lr=0.09, momentum=0.2)
         self.model.compile(optimizer=optimizer, loss='logcosh')
 
         if path is not None:
@@ -89,16 +89,21 @@ class Brain:
 
     def process_buffer(self):
         np.random.shuffle(self.buffer)
+        self.targets = []
+        self.states = []
         for (s, a, r, s_prim) in self.buffer[:len(self.buffer)]:
-            q = self.model.predict(s)
-            target = q
-            q_prim = self.model.predict(s_prim)
-            # np.argmax(a[0]) = the action: s -> s_prim
-            # q_prim[0][0] = estimated reward starting from s_prim and apllying action 0
-            # q_prim = [0][1] = estimated reward starting from s_prim and apllying action 1
-            target[0][np.argmax(a[0])] = r + self.discount_factor * max(q_prim[0])
-            np.append(self.targets,target)
-            np.append(self.states,s)
+            if a is not None:
+                q = self.model.predict(s)
+                target = q
+                q_prim = self.model.predict(s_prim)
+                # np.argmax(a[0]) = the action: s -> s_prim
+                # q_prim[0][0] = estimated reward starting from s_prim and apllying action 0
+                # q_prim = [0][1] = estimated reward starting from s_prim and apllying action 1
+                target[0][np.argmax(a[0])] = r + self.discount_factor * max(q_prim[0])
+                self.targets.append(target[0])
+                self.states.append(s[0])
         # aici se produce eroare, uitate si la __init__ cum am initalizat vectorii, posibil/PROBABIL am gresit pe acolo.
-        self.model.fit(self.states, self.targets, batch_size=32)
+        self.targets = np.array(self.targets)
+        self.states = np.array(self.states)
+        self.model.fit(self.states, self.targets, batch_size=100)
         self.buffer.clear()
