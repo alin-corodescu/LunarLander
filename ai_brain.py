c597bdc46a0bbc634fb361311d2eab803f05d133
alin-corodescu <alin.corodescu@gmail.com>
2017-12-20 21:34:06 +0200

    redone the buffer sampling

--- ai_brain.py
+++ ai_brain.py
@@ -1,3 +1,4 @@
+import random
 import time
 
 import numpy as np
@@ -15,13 +16,16 @@ class Brain:
     action = None
     epsilon = 1
     epsilon_decay_counter = 1
-
-    MAX_BUFFER = 10000
-    BATCH_SIZE = 200
+    epsilon_start = 1
+    epsilon_end = 0.05
+    initial_random_actions = 500
+    MAX_BUFFER = 5000
+    BATCH_SIZE = 32
     model = None
-    discount_factor = 0.75
+    target_estimator = None
+    discount_factor = 0.99
     buffer = []
-
+    random_obs = 0
     start_time = int(time.time())
     serialize_file_destination = "log/model"
     serialize_time_interval = 3600
@@ -41,11 +45,14 @@ class Brain:
             raise RuntimeError("The serialization interval must be a positive number!\n")
 
     # Will pass the input through the network and output an action
-    # This is a random action for the moment
-    def get_action_for(self, current_observation):
+    def get_action_for(self, current_observation, training):
         self.state = self.format_input(current_observation)
-        self.epsilon_decay()
-        if np.random.choice([True, False], 1, p=[self.epsilon, 1 - self.epsilon]):
+        if self.random_obs > self.initial_random_actions:
+            # print("epsilon begins decaying")
+            self.epsilon_decay()
+        else:
+            self.random_obs += 1
+        if np.random.choice([True, False], 1, p=[self.epsilon, 1 - self.epsilon]) and training:
             self.action = None
             self.rand_action = self.action_space.sample()
             return self.rand_action
@@ -59,11 +66,16 @@ class Brain:
     # At the beginning we take random actions
     # As we gain experience, we lower the posibility of taking random actions
     def epsilon_decay(self):
-        if self.epsilon_decay_counter <= 100:
-            if self.epsilon_decay_counter % 5 is 0:
-                self.epsilon -= 0.5
-        else:
-            self.epsilon = 0.05
+        if self.epsilon_decay_counter % 100 == 0:
+            # print("epsilon decayed")
+            self.epsilon -= 0.05
+
+        # stop decaying after a point
+        if self.epsilon_decay_counter < 2000:
+            self.epsilon_decay_counter += 1
+
+        if self.epsilon < self.epsilon_end:
+            self.epsilon = self.epsilon_end
 
 
     def __init__(self, action_space, observation_space, path=None):
@@ -76,7 +88,7 @@ class Brain:
         self.model.add(Dense(64))
         self.model.add(Activation('relu'))
         self.model.add(Dense(action_space.n))
-        self.model.add(Activation('linear'))
+        # self.model.add(Activation('linear'))
 
         optimizer = SGD(lr=0.05)
         self.model.compile(optimizer=optimizer, loss='logcosh')
@@ -87,14 +99,15 @@ class Brain:
     # Stores the reward from the last action outputted by this Brain, used to train the neural network
     # As an optimization, we could batch the updates (see course slides)
 
-    def put_reward(self, reward, new_state):
+    def put_reward(self, reward, new_state, done):
         # if action is NOT random, then we train the neural network
         # if(type(self.action) == 'list'):
         #     return
-        self.buffer.append((self.state, self.action, reward, self.format_input(new_state)))
-
-        if len(self.buffer) == self.MAX_BUFFER:
-            self.process_buffer()
+        # If we need to kick out the first element
+        if (len(self.buffer) == self.MAX_BUFFER):
+            self.buffer.pop(0)
+        self.buffer.append((self.state, self.action, reward, self.format_input(new_state), done))
+        self.process_buffer()
 
     # Loads a serialized model from the disk
     def load_model(self, path):
@@ -107,13 +120,14 @@ class Brain:
         return current_observation.reshape(1, 8)
 
     def process_buffer(self):
-        np.random.shuffle(self.buffer)
+        if (len(self.buffer) > self.BATCH_SIZE):
+            samples = random.sample(self.buffer, self.BATCH_SIZE)
+        else:
+            return
         self.targets = []
         self.states = []
         self.tar_arg = 0
-        if len(self.buffer) == 0:
-            return
-        for (s, a, r, s_prim) in self.buffer[:len(self.buffer)//3]:
+        for (s, a, r, s_prim, done) in samples:
             if a is not None:
                 self.tar_arg = np.argmax(a[0])
             else:
@@ -124,11 +138,10 @@ class Brain:
             # np.argmax(a[0]) = the action: s -> s_prim
             # q_prim[0][0] = estimated reward starting from s_prim and apllying action 0
             # q_prim = [0][1] = estimated reward starting from s_prim and apllying action 1
-            target[0][self.tar_arg] = r + self.discount_factor * max(q_prim[0])
+            target[0][self.tar_arg] = r + self.discount_factor * max(q_prim[0]) if not done else 0
             self.targets.append(target[0])
             self.states.append(s[0])
         # aici se produce eroare, uitate si la __init__ cum am initalizat vectorii, posibil/PROBABIL am gresit pe acolo.
         self.targets = np.array(self.targets)
         self.states = np.array(self.states)
-        self.model.fit(self.states, self.targets, batch_size=self.BATCH_SIZE)
-        self.buffer.clear()
+        self.model.fit(self.states, self.targets)
